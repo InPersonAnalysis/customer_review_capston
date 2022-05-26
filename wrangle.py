f97281da5ec400732380dd288898bd789dd4400d
craigcalzado <96085268+craigcalzado@users.noreply.github.com>
2022-05-26 12:14:59 -0500

    Updated NLP functions

--- wrangle.py
+++ wrangle.py
@@ -42,3 +42,115 @@ def wrangle_hotel(df):
                                      .split(','))] for tags in df.tags]
     
     return df
+
+# NLP PREPERATION
+import nltk
+import unicodedata
+import re
+from nltk.corpus import stopwords
+
+def basic_clean(string):
+    '''
+    This function takes in a string and
+    returns the string normalized.
+    '''
+    string = unicodedata.normalize('NFKD', string)\
+             .encode('ascii', 'ignore')\
+             .decode('utf-8', 'ignore')
+    string = re.sub(r'[^\w\s]', '', string).lower()
+
+    return string
+
+def tokenize(string):
+    '''
+    This function takes in a string and
+    returns a tokenized string.
+    '''
+    # Create tokenizer.
+    tokenizer = nltk.tokenize.ToktokTokenizer()
+    
+    # Use tokenizer
+    string = tokenizer.tokenize(string, return_str = True)
+
+    return string
+
+def stem(string):
+    '''
+    This function takes in a string and
+    returns a string with words stemmed.
+    '''
+    # Create porter stemmer.
+    ps = nltk.porter.PorterStemmer()
+    
+    # Use the stemmer to stem each word in the list of words we created by using split.
+    stems = [ps.stem(word) for word in string.split()]
+    
+    # Join our lists of words into a string again and assign to a variable.
+    string = ' '.join(stems)
+    
+    return string
+
+def lemmatize(string):
+    '''
+    This function takes in string for and
+    returns a string with words lemmatized.
+    '''
+    # Create the lemmatizer.
+    wnl = nltk.stem.WordNetLemmatizer()
+    
+    # Use the lemmatizer on each word in the list of words we created by using split.
+    lemmas = [wnl.lemmatize(word) for word in string.split()]
+    
+    # Join our list of words into a string again and assign to a variable.
+    string = ' '.join(lemmas)
+    
+    return string
+
+from nltk.corpus import stopwords
+
+def remove_stopwords(string, extra_words = [], exclude_words = []):
+    '''
+    This function takes in a string, optional extra_words and exclude_words parameters
+    with default empty lists and returns a string.
+    '''
+    # Create stopword_list.
+    stopword_list = stopwords.words('english')
+    
+    # Remove 'exclude_words' from stopword_list to keep these in my text.
+    stopword_list = set(stopword_list) - set(exclude_words)
+    
+    # Add in 'extra_words' to stopword_list.
+    stopword_list = stopword_list.union(set(extra_words))
+
+    # Split words in string.
+    words = string.split()
+    
+    # Create a list of words from my string with stopwords removed and assign to variable.
+    filtered_words = [word for word in words if word not in stopword_list]
+    
+    # Join words in the list back into strings and assign to a variable.
+    string_without_stopwords = ' '.join(filtered_words)
+    
+    return string_without_stopwords
+# One and done fuction for NLP
+def nlp_clean(df):
+    '''
+    This function takes in a Customer Review Data and returns NLP prep.
+    '''
+    # Apply basic clean and tokenize to each review.
+    df['positive_clean_review'] = df['positive_review'].apply(basic_clean)
+    df['negative_clean_review'] = df['negative_review'].apply(basic_clean)
+
+    df['positive_clean_review'] = df['positive_clean_review'].apply(tokenize)
+    df['negative_clean_review'] = df['negative_clean_review'].apply(tokenize)
+
+    df['positive_clean_review'] = df['positive_clean_review'].apply(remove_stopwords)
+    df['negative_clean_review'] = df['negative_clean_review'].apply(remove_stopwords)
+    # Apply stem to each review.
+    df['positive_stem'] = [stem(review) for review in df.positive_clean_review]
+    df['negative_stem'] = [stem(review) for review in df.negative_clean_review]
+    # Apply lemmatize to each review.
+    df['positive_lemma'] = [lemmatize(review) for review in df.positive_clean_review]
+    df['negative_lemma'] = [lemmatize(review) for review in df.negative_clean_review]
+    return df
+#### END NLP PREPERATION
